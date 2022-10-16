bdd872030c79b8e9339494aaa42cf269bb338446
Sebastien Dethyre <72963755+SebastienDethyre@users.noreply.github.com>
2022-10-16 20:19:04 +0200

    Add files via upload

--- Chrono/assets/js/StopWatch.js
+++ Chrono/assets/js/StopWatch.js
@@ -19,22 +19,33 @@ export class StopWatch {
         this.#startButton = createElement("span", {id:"startButton", class : "button" }, this.#element );
     }
     
-    #condition(i) {
+    #specialCondition(i) {
         console.log(`Current : ${i-1}`);
         return i === 0;
+    }
+    
+    #normalCondition(i) {
+        console.log(`Current : ${i-1}`);
+        return i === 1;
     }
     
     #addEventListeners(){
         const countDown = async () => {
             console.log("Start");
-            this.#startButton.removeEventListener("click", countDown)
-       
-            await this.#secondsCounter.autoDecrementCounter(this.#condition, 1000);
+            this.#startButton.removeEventListener("click", countDown);
+
+            if (this.#minutesCounter.getValue() !== 0 && this.#secondsCounter.getValue() === 0) 
+                 await this.#secondsCounter.autoDecrementCounter(this.#specialCondition , 1000);
+            else await this.#secondsCounter.autoDecrementCounter(this.#normalCondition  , 1000);
+
             while (this.#minutesCounter.getValue() > 0) {
                 this.#minutesCounter.setCounter("-", 1);
                 this.#secondsCounter.setCounter("+", 60);
-                await this.#secondsCounter.autoDecrementCounter(this.#condition, 1000);
-            }
+                if (this.#minutesCounter.getValue() !== 0 && this.#secondsCounter.getValue() === 0) 
+                     await this.#secondsCounter.autoDecrementCounter(this.#specialCondition , 1000);
+                else await this.#secondsCounter.autoDecrementCounter(this.#normalCondition  , 1000);
+            }               
+           
             this.#startButton.addEventListener("click", countDown)
             console.log("Countdown done !");
         }
